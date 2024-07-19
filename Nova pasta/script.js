bebdb47ef0a12befe92efa218282c8de9be36bd1
MineNeuron <159576873+MineNeuron@users.noreply.github.com>
2024-07-19 17:31:41 -0300

    Update script.js

--- Nova pasta/script.js
+++ Nova pasta/script.js
@@ -48,3 +48,5 @@ document.addEventListener('DOMContentLoaded', () => {
         userPlacement.textContent = 'Classificado';
     }
 });
+window.addEventListener("contextmenu", function(e) { e.preventDefault(); })
+
